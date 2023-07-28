7f44b3ede04a5dd9478919e039fe605dd0ad0d6f
Gaby <gabrielanicole@unitec.edu>
2023-07-28 13:52:10 -0600

    Se creó Clase Plan

--- src/herencia_progralab2/Plan.java
+++ src/herencia_progralab2/Plan.java
@@ -8,6 +8,28 @@ package herencia_progralab2;
  *
  * @author Gabriela Mejía
  */
-public class Plan {
+
+public abstract class Plan {
+    private int numeroTelefono;
+    private String nombreCliente;
+
+    Plan(int numeroTelefono, String nombreCliente) {
+        this.numeroTelefono = numeroTelefono;
+        this.nombreCliente = nombreCliente;
+    }
+
+    int getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    abstract double pagoMensual(int mins, int msgs);
     
+    void imprimir() {
+        System.out.println("Número de teléfono: " + numeroTelefono);
+        System.out.println("Nombre del cliente: " + nombreCliente);
+    }
 }
