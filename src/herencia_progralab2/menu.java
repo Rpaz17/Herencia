8bbd51980ab764b8139dfde4e444b5accf06166b
pcast <pcast@DESK-E>
2023-07-28 14:03:59 -0600

    swing menu

--- src/herencia_progralab2/menu.java
+++ src/herencia_progralab2/menu.java
@@ -26,21 +26,79 @@ public class menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        agregarPlan = new javax.swing.JButton();
+        pago = new javax.swing.JButton();
+        agregarAmigo = new javax.swing.JButton();
+        salir = new javax.swing.JButton();
+        listar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        agregarPlan.setFont(new java.awt.Font("Segoe UI Semilight", 0, 18)); // NOI18N
+        agregarPlan.setText("AGREGAR PLAN");
+        agregarPlan.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        pago.setFont(new java.awt.Font("Segoe UI Semilight", 0, 18)); // NOI18N
+        pago.setText("PAGO DE PLAN");
+        pago.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        agregarAmigo.setFont(new java.awt.Font("Segoe UI Semilight", 0, 18)); // NOI18N
+        agregarAmigo.setText("AGREGAR AMIGO");
+        agregarAmigo.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        salir.setFont(new java.awt.Font("Segoe UI Semilight", 0, 18)); // NOI18N
+        salir.setText("SALIR");
+        salir.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        salir.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                salirMouseClicked(evt);
+            }
+        });
+
+        listar.setFont(new java.awt.Font("Segoe UI Semilight", 0, 18)); // NOI18N
+        listar.setText("LISTAR");
+        listar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 500, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(146, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(agregarPlan, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(pago, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(agregarAmigo, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addGap(180, 180, 180))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(192, 192, 192)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(listar)
+                    .addComponent(salir))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 720, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(92, 92, 92)
+                .addComponent(agregarPlan)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(pago)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(agregarAmigo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(listar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(salir)
+                .addContainerGap(53, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void salirMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_salirMouseClicked
+        System.exit(0);
+    }//GEN-LAST:event_salirMouseClicked
 
     /**
      * @param args the command line arguments
@@ -78,5 +136,10 @@ public class menu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton agregarAmigo;
+    private javax.swing.JButton agregarPlan;
+    private javax.swing.JButton listar;
+    private javax.swing.JButton pago;
+    private javax.swing.JButton salir;
     // End of variables declaration//GEN-END:variables
 }
