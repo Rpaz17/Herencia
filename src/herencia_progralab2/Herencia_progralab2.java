abdbd2a44e6c12cbaa474518fc28891fe96b05d5
fampa <fampa@RebecaLT>
2023-07-28 13:44:44 -0600

    inicio prueba

--- src/herencia_progralab2/Herencia_progralab2.java
+++ src/herencia_progralab2/Herencia_progralab2.java
@@ -1,16 +1,9 @@
 package herencia_progralab2;
 
-/**
- *
- * @author fampa
- */
 public class Herencia_progralab2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Holaaa");
     }
     
 }
