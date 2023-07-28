7903d4eb701207e234637f4c48e87add3c7b97b2
Gaby <gabrielanicole@unitec.edu>
2023-07-28 14:58:34 -0600

    Se creó clase Tigo

--- src/herencia_progralab2/Tigo.java
+++ src/herencia_progralab2/Tigo.java
@@ -8,6 +8,86 @@ package herencia_progralab2;
  *
  * @author Gabriela Mejía
  */
+import java.util.ArrayList;
+
 public class Tigo {
+    private ArrayList<Plan> planes;
+
+    public Tigo() {
+        planes = new ArrayList<>();
+    }
+
+    public void agregarPlan(int numeroTel, String nombre, String extra, String tipo) {
+        if (busqueda(numeroTel, extra, tipo)) {
+            System.out.println("El numero de telefono ya existe. Dato ya registrado.");
+            return;
+        }
+        if (tipo.equals("IPHONE")) {
+            planes.add(new PlanIPhone(numeroTel, nombre, extra));
+        } else if (tipo.equals("SAMSUNG")) {
+            planes.add(new PlanSamsung(numeroTel, nombre, extra));
+        } else {
+            System.out.println("Tipo de plan no valido.");
+        }
+    }
+
+    public boolean busqueda(int numeroTel, String datoExtra, String tipo) {
+        for (Plan plan : planes) {
+            if (plan.getNumeroTelefono() == numeroTel) {
+                return true;
+            }
+            if (tipo.equals("IPHONE") && plan instanceof PlanIPhone) {
+                PlanIPhone planIPhone = (PlanIPhone) plan;
+                if (planIPhone.getEmail().equals(datoExtra)) {
+                    return true;
+                }
+            } else if (tipo.equals("SAMSUNG") && plan instanceof PlanSamsung) {
+                PlanSamsung planSamsung = (PlanSamsung) plan;
+                if (planSamsung.getPin().equals(datoExtra)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     
+    public double pagoPlan(int numeroTel, int mins, int msgs) {
+        for (Plan plan : planes) {
+            if (plan.getNumeroTelefono() == numeroTel) {
+                return plan.pagoMensual(mins, msgs);
+            }
+        }
+        System.out.println("El numero de telefono no existe.");
+        return 0.0;
+    }
+
+    public void agregarAmigo(int numeroTel, String pin) {
+        for (Plan plan : planes) {
+            if (plan.getNumeroTelefono() == numeroTel && plan instanceof PlanSamsung) {
+                PlanSamsung planSamsung = (PlanSamsung) plan;
+                planSamsung.agregarPinAmigo(pin);
+                return;
+            }
+        }
+        System.out.println("El numero de telefono no existe o el plan no es de tipo SAMSUNG.");
+    }
+
+    public void lista() {
+        int countSamsung=0;
+        int countIPhone=0;
+        
+        for (Plan plan : planes) {
+            plan.imprimir();
+            System.out.println("Pago Mensual: $" + plan.pagoMensual(100, 50));
+            System.out.println();
+
+            if (plan instanceof PlanSamsung) {
+                countSamsung++;
+            } else if (plan instanceof PlanIPhone) {
+                countIPhone++;
+            }
+        }
+        System.out.println("Total de planeas Samsung: "+countSamsung);
+        System.out.println("Total de planes IPhone: "+countIPhone);
+    }
 }
