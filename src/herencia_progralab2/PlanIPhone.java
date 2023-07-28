f1d21821a676d778b38fd149aff74396cb4fb623
fampa <fampa@RebecaLT>
2023-07-28 14:36:23 -0600

    se creo clase PlanIPhone (terminado)

--- src/herencia_progralab2/PlanIPhone.java
+++ src/herencia_progralab2/PlanIPhone.java
@@ -1,5 +1,27 @@
 package herencia_progralab2;
 
-public class PlanIPhone {
+import java.util.ArrayList;
+
+public class PlanIPhone extends Plan {
+     private String email;
+    public PlanIPhone(int numeroTelefono, String nombreCliente, String email){
+        super(numeroTelefono, nombreCliente);
+        this.email=email;
+    }
+
+    @Override
+    double pagoMensual(int mins, int msgs) {
+        double costoMinutos = 0.4*mins;
+        double costoMensajes = 0.1*msgs;
+       return (22+costoMinutos+costoMensajes);
+    }
     
+    void imprimir(){
+        super.imprimir();
+        System.out.println(email);
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
